4acf7f017e32f23472e55a97a9ff93c55860761b
mariosk <marios.koulakis@gmail.com>
2019-06-10 11:49:48 +0200

    feat: Define the forward pass of the decoder

--- image-captioning/project/model.py
+++ image-captioning/project/model.py
@@ -23,10 +23,36 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super().__init__()
+        
+        self.hidden_size = hidden_size
+        
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        
+        self.lstm = nn.LSTM(
+            embed_size, 
+            hidden_size, 
+            num_layers, 
+            batch_first=True)
+        
+        self.fc = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        embedded_features_and_captions = torch.cat(
+            (features.unsqueeze(1),
+             self.embedding(captions[:,:-1])),
+            dim=1)
+        
+        batch_size = features.shape[0]
+        hidden = (
+            torch.randn(1, batch_size, self.hidden_size), 
+            torch.randn(1, batch_size, self.hidden_size))
+        
+        output, _ = self.lstm(embedded_features_and_captions, hidden)
+        
+        return self.fc(output)
+            
+        
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
